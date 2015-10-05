0392041cc6b7c369cda0aef3703ae597b4fb3071
yflement <youri.flement@gmail.com>
2015-10-05 10:03:52 +0200

    Basic styling and validation of the login form.

--- web-app/src/main/java/org.realdolmen.webbroker/controller/LoginController.java
+++ web-app/src/main/java/org.realdolmen.webbroker/controller/LoginController.java
@@ -2,18 +2,21 @@ package org.realdolmen.webbroker.controller;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 @RequestScoped
 @Named
 public class LoginController implements Serializable {
 
+    @NotNull(message = "Please enter a username")
     private String username;
 
+    @NotNull(message = "Please enter a password")
     private String password;
 
     public void login() {
-
+        System.out.println("Login with: " + username + " " + password);
     }
 
     public String getPassword() {
