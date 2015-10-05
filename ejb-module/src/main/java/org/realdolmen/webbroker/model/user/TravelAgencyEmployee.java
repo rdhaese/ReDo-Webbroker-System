51adab9d8963534bbc573ab414bdb24224116742
Robin D'Haese <r.dhaese92@gmail.com>
2015-10-05 10:48:47 +0200

    Added @Entities

--- ejb-module/src/main/java/org/realdolmen/webbroker/model/user/TravelAgencyEmployee.java
+++ ejb-module/src/main/java/org/realdolmen/webbroker/model/user/TravelAgencyEmployee.java
@@ -2,10 +2,18 @@ package org.realdolmen.webbroker.model.user;
 
 import org.realdolmen.webbroker.model.TravelAgency;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
 /**
  * Created by RDEAX37 on 2/10/2015.
  */
+@Entity
 public class TravelAgencyEmployee extends User {
+
+    @NotNull
+    @ManyToOne
     private TravelAgency travelAgency;
 
     public TravelAgency getTravelAgency() {
--- ejb-module/src/main/java/org/realdolmen/webbroker/model/user/User.java
+++ ejb-module/src/main/java/org/realdolmen/webbroker/model/user/User.java
@@ -2,12 +2,61 @@ package org.realdolmen.webbroker.model.user;
 
 import org.realdolmen.webbroker.model.BaseEntity;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+
 /**
  * Created by RDEAX37 on 2/10/2015.
  */
+@Entity
 public class User extends BaseEntity {
+    @NotNull
     private String lastName;
+    @NotNull
     private String firstName;
+    @NotNull
+    private String userName;
+    @NotNull
     private String password;
     private String salt;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String username) {
+        this.userName = username;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
 }
