41e8e27d1db89aefe2b49da627053c1d19898749
yflement <youri.flement@gmail.com>
2015-10-08 13:44:48 +0200

    Added current booking controller.

---
+++ web-app/src/main/java/org/realdolmen/webbroker/controller/CurrentBookingController.java
@@ -0,0 +1,12 @@
+package org.realdolmen.webbroker.controller;
+
+
+import org.realdolmen.webbroker.model.Booking;
+
+public class CurrentBookingController {
+
+    public void setCurrentBooking(Booking booking) {
+
+    }
+
+}
